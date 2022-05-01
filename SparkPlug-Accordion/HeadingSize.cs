415eb10677edf80cdb7224a20eb9c7b48c64c37b
Benjamin Kent <benjaminfranklinkent@gmail.com>
2022-05-01 15:03:06 -0700

    Enabled folder identification

---
+++ SparkPlug-Accordion/HeadingSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkPlug_Accordion
+{
+    public enum HeadingSize
+    {
+        h1 = 1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+    }
+}
